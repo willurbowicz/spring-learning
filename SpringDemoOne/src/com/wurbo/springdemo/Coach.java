139ed77da561d03d69a7ac68bc6a5ebd21ae24fe
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-01-22 16:22:52 -0500

    Section 4: Rough Prototype
    Creating very basic coach implementation with pure java, no Spring yet

---
+++ SpringDemoOne/src/com/wurbo/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.wurbo.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+
+}
---
+++ SpringDemoOne/src/com/wurbo/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.wurbo.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k.";
+	}
+
+}
