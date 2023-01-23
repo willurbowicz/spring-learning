eb18cd7f6369f41583f930286ed60617bb7b243d
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-01-23 17:25:43 -0500

    Section 10: Spring Configuration with Java Code
    Learning to configure Spring beans with pure java code

---
+++ SpringDemoAnnotations/src/com/wurbo/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.wurbo.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.wurbo.springdemo")
+public class SportConfig {
+
+}
