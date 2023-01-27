6857999f2afa1ecdbe7e58eec3f4269263bc44e9
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-01-27 00:05:04 -0500

    Section 14: Drop-Down Lists
    Learning how to use Spring Select form to create a dropdown menu.

--- SpringMVCDemo/src/com/wurbo/springdemo/mvc/Student.java
+++ SpringMVCDemo/src/com/wurbo/springdemo/mvc/Student.java
@@ -1,11 +1,26 @@
 package com.wurbo.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
 	
+	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
+	
 	public Student() {
+		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");
 	}
 
 	public String getFirstName() {
@@ -22,6 +37,18 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 	
 }
