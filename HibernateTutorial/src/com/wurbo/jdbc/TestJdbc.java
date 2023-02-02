9daec8a9f126522cbca1d2e450f84aa9ea414560
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-02-02 00:06:54 -0500

    Section 19: Testing Your JDBC Connectionection
    Creating a simple test app to ensure database connection is working properly

---
+++ HibernateTutorial/src/com/wurbo/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.wurbo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = 
+					DriverManager.getConnection(jdbcUrl, user, password);
+			
+			System.out.println("Connection Successful!");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
