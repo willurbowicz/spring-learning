e6f5393d43ec6cfd11a0f8fde9beff6e2ff4c39d
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-02-16 21:49:34 -0500

    Section 30: Refactor: Add a Service layer
    Adding a service layer to our application

---
+++ WebCustomerTracker/src/com/wurbo/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.wurbo.springdemo.service;
+
+import java.util.List;
+
+import com.wurbo.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
---
+++ WebCustomerTracker/src/com/wurbo/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.wurbo.springdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wurbo.springdemo.dao.CustomerDAO;
+import com.wurbo.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
