9923a00e8c3420277a3ab2b37fc93c0df1d45d0a
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-02-28 23:22:55 -0500

    Section 40: AOP JoinPoints
    Learning to read method arguments with JoinPoints

---
+++ SpringDemoAopReadJoinPoint/src/com/wurbo/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,46 @@
+package com.wurbo.aopdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.wurbo.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@Before("com.wurbo.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		System.out.println("\n=====>>> Executing @Before advice on method");
+		
+		// display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+		
+		// display method arguments
+		
+		// get args
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop through args
+		for(Object tempArg : args) {
+			System.out.println(tempArg);
+			
+			if(tempArg instanceof Account) {
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+				
+				System.out.println("Account name: " + theAccount.getName());
+				System.out.println("Account level: " + theAccount.getLevel());
+
+			}
+		}
+	}
+	
+}
