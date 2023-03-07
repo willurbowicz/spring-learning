c456e6be4a13afa2456fa5930685ae01f8c6437f
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2023-03-06 23:11:27 -0500

    Section 45: AOP: Add AOP Logging to Spring MVC App
    Adding AOP Logging to the MVC Web app created earlier in the course

---
+++ WebCustomerTrackerAop/src/com/wurbo/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.wurbo.springdemo.dao;
+
+import java.util.List;
+
+import com.wurbo.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+}
